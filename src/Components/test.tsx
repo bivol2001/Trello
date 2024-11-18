9ab3da23d8b65169affb549258a2bcb59ebfdda7
Corneliu86 <corneliu86@gmail.com>
2024-11-18 18:04:31 +0200

    trello-dashboard

---
+++ src/Components/test.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface Janelprops {
+  text: string;
+  description?: string;
+  isDescription?: boolean;
+}
+
+const Janel = ({ text, description, isDescription = false }: Janelprops) => {
+  console.log(description);
+  return (
+    <div>
+      <h1>{text}</h1>
+      {isDescription ? <h2>{description ? description : "default"}</h2> : <></>}
+    </div>
+  );
+};
+
+export default Janel;
--- src/Pages/Page After log/HomepageLog.tsx
+++ src/Pages/Page After log/HomepageLog.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { useNavigate } from "react-router";
 import NavBarLog from "./NavBarLog";
+import Janel from "../../Components/test";
 
 const HomePageLog = () => {
-    return (<div>
-        <NavBarLog/>
-    </div> )
-}
-
+  return (
+    <div>
+      <NavBarLog />
+      <Janel text="janica" />
+      <Janel text="salut" description="" />
+      <Janel text="fata" description="tresa" isDescription />
+    </div>
+  );
+};
 
 export default HomePageLog;
